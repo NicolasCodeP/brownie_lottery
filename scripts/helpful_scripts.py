112c689943934cf329ba3e1a031e7afb4502c781
blueniarb <blueniarb@gmail.com>
2024-04-21 14:28:04 +0800

    Adding ERC677 TOKEN funding

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,9 +1,11 @@
 """ Helpful scripts """
+
 from brownie import (
     Contract,
     network,
     accounts,
     config,
+    interface,
     MockV3Aggregator,
     VRFCoordinatorV2Mock,
     # VRFCoordinatorV2Interface,
@@ -19,6 +21,28 @@ STARTING_PRICE = 200000000000
 # Used with Web3.toWei function
 # DECIMAL = 18
 # STARTING_PRICE = 2000
+
+
+def int_to_uint256(val: int):
+    """Encode integer to uint256
+
+    Args:
+        val (int): The integer to encode
+
+    >>> int_to_uint256(11238)
+    "0x0000000000000000000000000000000000000000000000000000000000002be6"
+    """
+    # Convert the integer to a 32-byte bytes object
+    # With the most significant byte first (big-endian order)
+    val_bytes = val.to_bytes(32, byteorder="big")
+
+    # Convert the bytes object to a hexadecimal string
+    val_hex = val_bytes.hex()
+
+    # Add the '0x' prefix and zero-pad to 64 characters
+    val_uint256 = "0x" + val_hex.zfill(64)
+
+    return val_uint256
 
 
 def get_account(index=None, id_=None):
@@ -100,10 +124,13 @@ def deploy_mocks():
 def fund_subscription(
     account=None,
     vrf_coordinator=None,
-    amount=1000000000000000000,  # 0.1 LINK
-    # amount=100000000000000000, # 0.1 LINK
+    amount=1000000000000000000,  # 1 LINK
+    min_balance=1,
 ):
-    """Create and fund subscription if testing
+    """
+    Trasnfer LINK token if not enough funds.
+    Use VRFCoordinatorV2Mock to fund subscription for local test.
+
 
     Returns:
         subscription_id:
@@ -125,6 +152,25 @@ def fund_subscription(
         subscription_id = config["networks"][network.show_active()][
             "vrf_subscriptionId"
         ]
+        subscription = vrf_coordinator.getSubscription(
+            subscription_id, {"from": account}
+        )
+        subscription_balance = int(subscription[0]) / (10**18)
+        print(f"Subscription {subscription_id} balance: {subscription_balance} LINK")
+        if subscription_balance < min_balance:
+            # Transfer LINK token
+            print(f"Not enough funds transferring {(amount / (10 ** 18))} LINK")
+            link_token_address = config["networks"][network.show_active()]["link_token"]
+            erc677 = interface.IERC677(link_token_address)
+            tx = erc677.transferAndCall(
+                vrf_coordinator.address,
+                amount,
+                int_to_uint256(subscription_id),
+                {"from": account},
+            )
+            tx.wait(1)
+            print(f"Funded: {(amount / (10 ** 18))} LINK")
+
     subscription = vrf_coordinator.getSubscription(subscription_id, {"from": account})
     return subscription_id, subscription
 
